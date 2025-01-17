17e943a2de85198d83f5d8c16e084ecad613965b
coder-gaia <alexandregaia911@gmail.com>
2025-01-17 19:33:43 -0300

    Saving a new department

--- src/main/java/gui/DepartmentListController.java
+++ src/main/java/gui/DepartmentListController.java
@@ -86,6 +86,7 @@ public class DepartmentListController implements Initializable {
             DepartmentFormController controller = loader.getController();
             controller.setDepartment(department);
             controller.updateFormData();
+            controller.setDepartmentService(new DepartmentService());
 
             Stage dialogStage = new Stage();
             dialogStage.setTitle("Enter department data");
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.javafx_project {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires jdk.jshell;
 
 
     exports application;
